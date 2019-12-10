6d8f440b732e682d8117ac7818b4db29d002348d
Craig Storer <craigstorer@gmail.com>
2019-12-10 03:52:46 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+
+def line(place)
+ number = []
+  if place >= 1
+    puts "The line is currently: #{x}. #{y}" #X and Y have to increase. #put 3 and break
+    break
+    
+  counter = 0
+  while counter == 0
+  puts "The line is currently empty."
+  break
+  counter += 1 
+end
+number
+end
+
+
+
+def take_a_number(katz_deli,name)
+  katz_deli = []
+  counter = 0
+  katz_deli.each_with_index(1) do |x,y|
+  
+  "Welcome, #{x}. You are number #{y} in line."
+  
+  counter == 0 
+  puts " The line is empty"
+end
+return katz_deli
+end
+  
+  
+def now_serving
+end
